9d02ded38826670fb98b41cfd5bd64d3d91de52b
bril <namejiahui1@gmail.com>
2026-09-07 23:01:21 +0800

    refactor: modernize image processing and excel styling with Go 1.23+ features
    - Use unique.Handle[RGB] for color value interning and style deduplication
    - Implement ImageRows iterator using iter.Seq2 for clean row-by-row streaming
    - Hoist SetColWidth outside the loop to execute only once
    - Set sheet DefaultRowHeight via SetSheetProps
    - Remove redundant goroutine closure parameter passing
    - Use Bounds().Dx() for accurate image width calculation

--- main.go
+++ main.go
@@ -5,16 +5,22 @@ import (
 	"image"
 	_ "image/jpeg"
 	_ "image/png"
+	"iter"
 	"log"
 	"os"
 	"sync"
 	"time"
+	"unique"
 
 	"github.com/xuri/excelize/v2"
 )
 
+type RGB struct {
+	R, G, B uint8
+}
+
 type colorRequest struct {
-	hexColor string
+	color    unique.Handle[RGB]
 	response chan int
 }
 
@@ -22,28 +28,34 @@ func main() {
 	start := time.Now()
 	image := loadImage(os.Args[1])
 	f := excelize.NewFile()
-	maxX, maxY := image.Bounds().Max.X, image.Bounds().Max.Y
+	maxX := image.Bounds().Dx()
 	colorChan := processBackgroundColor(f)
 	wg := &sync.WaitGroup{}
 
-	for y := 1; y <= maxY; y++ { // row
-		f.SetRowHeight("Sheet1", y, 1)
-		for x := 1; x <= maxX; x++ { //col
+	endCol, _ := excelize.ColumnNumberToName(maxX)
+	f.SetColWidth("Sheet1", "A", endCol, 0.01)
+
+	if err := f.SetSheetProps("Sheet1", &excelize.SheetPropsOptions{
+		DefaultRowHeight: new(1.0),
+		CustomHeight:     new(true),
+	}); err != nil {
+		log.Fatalf("failed to set sheet props: %v", err)
+	}
+
+	for y, row := range ImageRows(image) {
+		for x, colorHandle := range row {
 			wg.Add(1)
-			go func(x, y int) {
+			go func() {
 				defer wg.Done()
-				endCol, _ := excelize.ColumnNumberToName(maxX)
-				f.SetColWidth("Sheet1", "A", endCol, 0.01)
-				hexColor := getHexColor(image, x-1, y-1)
 				responseChan := make(chan int)
-				colorChan <- colorRequest{hexColor: hexColor, response: responseChan}
+				colorChan <- colorRequest{color: colorHandle, response: responseChan}
 				styleID := <-responseChan
-				cell, err := excelize.CoordinatesToCellName(x, y)
+				cell, err := excelize.CoordinatesToCellName(x+1, y+1)
 				if err != nil {
 					log.Fatalf("failed to get CellName: %v", err)
 				}
 				f.SetCellStyle("Sheet1", cell, cell, styleID)
-			}(x, y)
+			}()
 		}
 	}
 	wg.Wait()
@@ -64,28 +76,34 @@ func loadImage(imgPath string) image.Image {
 	return img
 }
 
-func getHexColor(img image.Image, x, y int) string {
+func getPixelColor(img image.Image, x, y int) unique.Handle[RGB] {
 	r, g, b, a := img.At(x, y).RGBA()
 	if a > 0 {
 		r = r * 0xffff / a
 		g = g * 0xffff / a
 		b = b * 0xffff / a
 	}
-	return fmt.Sprintf("#%02x%02x%02x", r>>8, g>>8, b>>8)
+	return unique.Make(RGB{
+		R: uint8(r >> 8),
+		G: uint8(g >> 8),
+		B: uint8(b >> 8),
+	})
 }
 
 func processBackgroundColor(f *excelize.File) chan colorRequest {
-	styles := make(map[string]int)
+	styles := make(map[unique.Handle[RGB]]int)
 	colorChan := make(chan colorRequest)
 	go func() {
 		for req := range colorChan {
-			styleID, exists := styles[req.hexColor]
+			styleID, exists := styles[req.color]
 			if !exists {
+				c := req.color.Value()
+				hexColor := fmt.Sprintf("#%02x%02x%02x", c.R, c.G, c.B)
 				style := &excelize.Style{
 					Fill: excelize.Fill{
 						Type:    "pattern",
 						Pattern: 1,
-						Color:   []string{req.hexColor},
+						Color:   []string{hexColor},
 					},
 				}
 				var err error
@@ -93,10 +111,27 @@ func processBackgroundColor(f *excelize.File) chan colorRequest {
 				if err != nil {
 					log.Fatalf("failed to NewStyle: %v", err)
 				}
-				styles[req.hexColor] = styleID
+				styles[req.color] = styleID
 			}
 			req.response <- styleID
 		}
 	}()
 	return colorChan
+}
+
+// ImageRows returns an iterator yielding (y, rowColors) for each row of the image.
+func ImageRows(img image.Image) iter.Seq2[int, []unique.Handle[RGB]] {
+	return func(yield func(int, []unique.Handle[RGB]) bool) {
+		bounds := img.Bounds()
+		width := bounds.Dx()
+		for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+			row := make([]unique.Handle[RGB], width)
+			for x := bounds.Min.X; x < bounds.Max.X; x++ {
+				row[x-bounds.Min.X] = getPixelColor(img, x, y)
+			}
+			if !yield(y-bounds.Min.Y, row) {
+				return
+			}
+		}
+	}
 }
